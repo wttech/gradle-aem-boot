056948466c7b5ade8be54184d7cbd5d4924787b1
Krystian Panek <krystian.panek@cognifide.com>
2019-12-03 16:33:08 +0100

    Boot improvements

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ defaultTasks(":setup")
 
 repositories {
     jcenter()
+    maven { url = uri( "https://dl.bintray.com/neva-dev/maven-public") }
 }
 
 aem {
@@ -60,32 +61,18 @@ aem {
     tasks {
         instanceSatisfy {
             packages {
-                "dep.vanity-urls" { /* useLocal("pkg/vanityurls-components-1.0.2.zip") */ }
-                "dep.acs-aem-commons" { download("https://github.com/Adobe-Consulting-Services/acs-aem-commons/releases/download/acs-aem-commons-4.0.0/acs-aem-commons-content-4.0.0-min.zip") }
-                "tool.ac-tool" { download("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool", "accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip", "accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip") }
-                "tool.aem-easy-content-upgrade" { download("https://github.com/valtech/aem-easy-content-upgrade/releases/download/2.0.0/aecu.bundle-2.0.0.zip") }
-                "tool.search-webconsole-plugin" { resolve("com.neva.felix:search-webconsole-plugin:1.2.0") }
+                 // "dep.vanity-urls"("pkg/vanityurls-components-1.0.2.zip")
+                "dep.acs-aem-commons"("https://github.com/Adobe-Consulting-Services/acs-aem-commons/releases/download/acs-aem-commons-4.0.0/acs-aem-commons-content-4.0.0-min.zip")
+                "tool.ac-tool"("https://repo1.maven.org/maven2/biz/netcentric/cq/tools/accesscontroltool", "accesscontroltool-package/2.3.2/accesscontroltool-package-2.3.2.zip", "accesscontroltool-oakindex-package/2.3.2/accesscontroltool-oakindex-package-2.3.2.zip")
+                "tool.aem-easy-content-upgrade"("https://github.com/valtech/aem-easy-content-upgrade/releases/download/2.0.0/aecu.bundle-2.0.0.zip")
+                "tool.search-webconsole-plugin"("com.neva.felix:search-webconsole-plugin:1.2.0")
+
             }
         }
 
         instanceProvision {
             // https://github.com/Cognifide/gradle-aem-plugin#task-instanceprovision
         }
-
-        // Here is a desired place for defining custom AEM tasks
-        // https://github.com/Cognifide/gradle-aem-plugin#implement-custom-aem-tasks
-
-        /*
-        register("doSomething") {
-            description = "Does something"
-            doLast {
-                aem.sync {
-                    // Use instance services:
-                    // http, repository, packageManager, osgiFramework, workflowManager, groovyConsole
-                }
-            }
-        }
-        */
     }
 }
 
