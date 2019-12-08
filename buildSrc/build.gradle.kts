beb3efdf0751b5e83828859f1b833f5b4efcdb5d
Krystian Panek <krystian.panek@cognifide.com>
2019-12-08 18:06:30 +0100

    Bump

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:9.1.3")
+    implementation("com.cognifide.gradle:aem-plugin:9.1.5")
     implementation("com.neva.gradle:fork-plugin:4.1.0")
 }
 
