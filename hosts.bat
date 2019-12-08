6549c829621776d3559657ec3c62e108e11d0881
Krystian Panek <krystian.panek@cognifide.com>
2019-12-08 18:07:23 +0100

    Hosts fix

--- hosts.bat
+++ hosts.bat
@@ -1,1 +1,7 @@
-powershell -command "Start-Process cmd -ArgumentList '/C cd %CD% && gradlew.bat environmentHosts --no-daemon' -Verb runas"
+del .gap.hosts.tmp
+
+echo gradlew.bat environmentHosts -q > .gap.hosts.tmp
+
+powershell -command "Start-Process cmd -ArgumentList '/C cd %CD% && type .gap.hosts.tmp >> C:\Windows\System32\drivers\etc\hosts' -Verb runas"
+
+del .gap.hosts.tmp
