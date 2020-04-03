882a4a2f704e807444b47f5c4b586efffe499fec
Krystian Panek <krystian.panek@cognifide.com>
2020-04-03 20:57:42 +0200

    Update build.gradle.kts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:12.0.0")
+    implementation("com.cognifide.gradle:aem-plugin:12.0.2")
     implementation("com.cognifide.gradle:environment-plugin:0.1.10")
     implementation("com.neva.gradle:fork-plugin:4.2.2")
 }
