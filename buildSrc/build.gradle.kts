36c10389f84ebeaba5b561975540dc5e7054c1b7
Krystian Panek <krystian.panek@cognifide.com>
2020-04-24 07:29:54 +0200

    GAP 13 release

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:13.0.0-beta")
+    implementation("com.cognifide.gradle:aem-plugin:13.0.0")
     implementation("com.cognifide.gradle:environment-plugin:0.1.12")
     implementation("com.neva.gradle:fork-plugin:4.2.2")
 }
