6c826251d0201ee4b88a8dd38daaf42c9b574f1f
Krystian Panek <krystian.panek@cognifide.com>
2020-04-27 11:24:31 +0200

    GAP upgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,8 +5,8 @@ repositories {
 }
 
 dependencies {
-    implementation("com.cognifide.gradle:aem-plugin:13.0.0")
+    implementation("com.cognifide.gradle:aem-plugin:13.0.2")
     implementation("com.cognifide.gradle:environment-plugin:0.1.12")
-    implementation("com.neva.gradle:fork-plugin:4.2.2")
+    implementation("com.neva.gradle:fork-plugin:5.0.0")
 }
 
